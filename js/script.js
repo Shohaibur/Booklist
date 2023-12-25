5fd5f66f1761b32bf27620ff322d1180b433cd23
Shoaib <shoaibrochi9@gmail.com>
2023-12-25 23:53:38 +0600

    add remove book functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 //ui elements
 let form = document.querySelector("#book-form");
-
-
+let bookList = document.querySelector("#book-list");
 
 class Book {
   constructor(title, author, isbn) {
@@ -11,28 +10,25 @@ class Book {
   }
 }
 
-//ui class
 class UI {
-  constructor() {
-
-  } 
-  addToBooklist(book) {
+  constructor() {}
+  addToBookList(book) {
     let list = document.querySelector("#book-list");
     let row = document.createElement("tr");
     row.innerHTML = `<td>${book.title}</td>
    <td>${book.author}</td>
    <td>${book.isbn}</td>
    <td><a href='#' class="delete">X</a></td>`;
-
     list.appendChild(row);
     //console.log(row);
+  }
 
-  };
   clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
     document.querySelector("#isbn").value = "";
   }
+
   showAlert(message, className) {
     let div = document.createElement("div");
     div.className = `alert ${className}`;
@@ -40,16 +36,20 @@ class UI {
     let container = document.querySelector(".container");
     let form = document.querySelector("#book-form");
     container.insertBefore(div, form); //insert div before form
-
-    setTimeout(function() {
-      document.querySelector('.alert').remove();
+    setTimeout(function () {
+      document.querySelector(".alert").remove();
     }, 3000);
+  }
+
+  deleteFromBook(target) {
+    if (target.hasAttribute("href")) {
+      target.parentNode.parentNode.remove(); //parentElement
+    }
   }
 }
 
-
-//event listeners
 form.addEventListener("submit", newBook);
+bookList.addEventListener("click", removeBook);
 
 function newBook(e) {
   let title = document.querySelector("#title").value,
@@ -59,19 +59,22 @@ function newBook(e) {
   let ui = new UI();
 
   if (title === "" || author === "" || isbn === "") {
-    ui.showAlert("Please fill all the fields!","error");
-
+    ui.showAlert("Please fill all the fields!", "error");
   } else {
     let book = new Book(title, author, isbn);
-
-    ui.addToBooklist(book);
-
+    ui.addToBookList(book);
     ui.clearFields();
-    ui.showAlert("Book addded","success");
-  };
-
+    ui.showAlert("Book added", "success");
+  }
 
   //console.log(book)
   //console.log("hello");
+  e.preventDefault();
+}
+
+function removeBook(e) { //todo alerts removed when clicked whole added body
+  let ui = new UI();
+  ui.deleteFromBook(e.target);
+  ui.showAlert("Book Removed!", "success");
   e.preventDefault();
 }
